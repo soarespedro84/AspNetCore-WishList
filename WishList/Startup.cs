997034f6e27030ca023070fc80890b515f3f371e
Pedro Soares <p.soares84@gmail.com>
2021-12-25 16:32:54 +0000

    Update

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -34,7 +34,6 @@ namespace WishList
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            
             app.UseRouting();
             app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
